857137593c1e709a596cd4fa4df08f99d60fe71f
midnight.walker <from.midnight.walker@gmail.com>
2016-09-15 00:03:40 +0900

    Changed: added comments          added requirements and how to use (need add more...)

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # slack_emoji_mover
 Move your Slack team emoji to other Slack team.
+
+##What is this script for?
+Move emoji from Slack team to other Slack team.
+
+##What do I need to use this script?
+*Enviropment:*  
+ +Ruby 2.2.2 or Above (tested by 2.2.2, so may work other version)  
+ +Ruby Gem: Mechanize
+ +Ruby Gem: json
+
+##How to use this script?
+Move all files to working folder, write all infomation to config file and
+run import_emoji.rb.
+Wait until all emoji data are uploaded.
+Done!
+
--- import_emoji.rb
+++ import_emoji.rb
@@ -2,27 +2,44 @@ require 'mechanize'
 require 'open-uri'
 require 'json'
 
+# Class to get emoji data from target slack team and upload emoji to
+# destination slack team
+#
 class SlackEmoji
   # variables
+  # file names
   ConfigFile = 'emoji_conf.txt'
   DefaultFile = 'default_emoji.txt'
   EmojiFile = 'emoji.txt'
   LogFile = 'emoji_log.txt'
+  # messages to judge upload was success or fail
   LoginFail = "Sorry, you entered an incorrect email address or password"
   AddSuccess = "Your new emoji has been saved"
   DuplicateEmoji = 'There is already an emoji named'
-  ApiUrl = 'https://slack.com/api/emoji.list?pretty=1&token=' # Slack emoji list API
-  @@url = '' # Slack team URL to import emoji
+  # Slack emoji list API
+  ApiUrl = 'https://slack.com/api/emoji.list?pretty=1&token='
+
+  # variables from config file
+  @@url = ''
   @@email = ''
   @@password = ''
   @@token = ''
+
+  # default emoji
   @@default = nil # default emoji set
+  # emoji JSON
   @@hash = nil
 
+  # Login to Slack team and upload all emoji by using Mechanize
+  #
+  # @return int -1 error
+  #              0 success 
   def uploadEmoji()
     log_file = open(LogFile, 'wb')
     agent = Mechanize.new
     agent.user_agent = 'Windows Mozilla'
+
+    # login to Slack team
     agent.get(@@url) do |page|
       response = page.form_with(:action => '/') do |form|
         formdata = {
@@ -33,12 +50,14 @@ class SlackEmoji
         form.field_with(:name => 'password').value = formdata[:password]
       end.submit
 
+      # if login fails, write error to log file and exit
       if response.code != '200' || response.body.include?(LoginFail)
         log_file.write("Login failed! Please check Slack url, email and password.\n")
         return -1
       end
       log_file.write("Login success!\n")
 
+      # loop upload emoji
       @@hash.each do |node|
         name = File.basename(node[1])
         agent.get(@@url + 'customize/emoji') do |emoji|
@@ -48,29 +67,34 @@ class SlackEmoji
               eform.radiobuttons_with(:name => 'mode')[0].check
               eform.file_upload_with(:name => 'img').file_name = './' + name
             end.submit
-            if eresponse.code != '200'
+	    # write result to log
+	    # check responce code and body to decide success or failer
+            if eresponse.code != '200' # check 
               log_file.write("F Name:[" + node[0] + "] FileName:[" + name + "] Result: ")
               log_file.write("Respose code is not 200. Failed to add emoji.\n")
-            elsif eresponse.body.include?(AddSuccess)
+            elsif eresponse.body.include?(AddSuccess) # add success log
               log_file.write("S Name:[" + node[0] + "] FileName:[" + name + "] Result: ")
               log_file.write("Successfully added.\n")
-            elsif eresponse.body.include?(DuplicateEmoji)
+            elsif eresponse.body.include?(DuplicateEmoji) # add error log - duplicate error
               log_file.write("F Name:[" + node[0] + "] FileName:[" + name + "] Result: ")
               log_file.write("Same emoji name already exist.\n")
-            else
+            else # add error log - unknown error
               log_file.write("F Name:[" + node[0] + "] FileName:[" + name + "] Result: ")
               log_file.write("Unknown error occured.\n")
             end
-          else
+          else # add error log - file not found
             log_file.write("F Name:[" + node[0] + "] FileName:[" + name + "] Result: ")
             log_file.write("File not exist.\n")
           end
         end
-        sleep 3
+        sleep 3 # wait 3 seconds for slack server
       end
     end
+    return 0 # success
   end
 
+  # Get team emoji text from Web API by JSON format
+  #
   def getEmojiText
     open(EmojiFile, 'wb') do |output|
       open(ApiUrl + @@token) do |data|
@@ -79,13 +103,15 @@ class SlackEmoji
     end
   end
 
-  def getEmojiData()
-    @@hash.each do |node|
-      name = File.basename(node[1])
+  # Download emoji file from URL
+  # must call readEmojiJSON before call this function
+  def getEmojiData
+    @@hash.each do |node| # loop each node
+      name = File.basename(node[1]) # get file name from URL
       if name !~ /^alias/
         open(name, 'wb') do |output|
           open(node[1]) do |data|
-            output.write(data.read)
+            output.write(data.read) # write to local
           end
         end
       end
@@ -93,6 +119,8 @@ class SlackEmoji
     end
   end
 
+  # Read emoji text and load to JSON
+  #
   def readEmojiJSON
     File.open(EmojiFile, 'r') do |file|
       @@hash = JSON.load(file)
@@ -106,6 +134,8 @@ class SlackEmoji
     end
   end
 
+  # Read config file
+  #
   def readConfigFile
     config = Array.new
     File.open(ConfigFile, 'r') do |file|
@@ -116,12 +146,14 @@ class SlackEmoji
         end
       end
     end
-    @@url = config[0]
-    @@email = config[1]
-    @@password = config[2]
-    @@token = config[3]
+    @@url = config[0] # target slack team URL 
+    @@email = config[1] # email to login
+    @@password = config[2] # password
+    @@token = config[3] # target slack team token 
   end
 
+  # read default emoji file
+  # some emoji are default, so need skip those
   def readDefaultEmojiFile
     @@default = Array.new
     File.open(DefaultFile, 'r') do |file|
@@ -135,14 +167,19 @@ class SlackEmoji
   end
 end
 
-
+# create object
 emoji = SlackEmoji.new
 
+# read config and default files
 emoji.readConfigFile
 emoji.readDefaultEmojiFile
 
+# get emoji text from target slack team
 emoji.getEmojiText
+# read emoji file
 emoji.readEmojiJSON
+# get all emoji data
 emoji.getEmojiData
+# upload emoji data
 emoji.uploadEmoji
 
